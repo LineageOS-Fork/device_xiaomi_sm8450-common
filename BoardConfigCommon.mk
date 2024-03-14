7ac8969fff538f26aa7764bbda914d2da3e41a81
althafvly <althafvly@gmail.com>
2024-03-14 20:01:41 +0530

    sm8450-common: Set `BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE`

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -239,6 +239,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # VINTF
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
